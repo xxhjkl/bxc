b52fcee7e78636914318e3bd3192483db6dbda25
xxhjkl <2010231040015@std.uestc.edu.cn>
2019-08-15 21:35:57 +0800

    Update bxc-back.sh

--- bxc-back.sh
+++ bxc-back.sh
@@ -2,5 +2,7 @@
 apt-get update
 apt-get install curlftpfs tar -y
 curlftpfs -o codepage=gbk ftp://账户:密码@IP:端口 /mnt
-tar -czvf /mnt/bcode/$(cat /var/lib/docker/volumes/bxc_data/_data/node.db | awk -F '"' '{print $12}').tar.gz -C /var/lib/docker/volumes/ bxc_data
+IP=$(ifconfig `ip route | grep default |awk '{print $5}'` | grep "inet " |awk '{print $2}')
+MAC=$(cat /var/lib/docker/volumes/bxc_data/_data/node.db | awk -F '"' '{print $12}')
+tar -czvf /mnt/bcode/"$IP"-"$MAC".tar.gz -C /var/lib/docker/volumes/ bxc_data
 umount /mnt
