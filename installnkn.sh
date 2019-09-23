3110d50bca1b5b2c5b3769e2716e5d7d96b319ea
xxhjkl <2010231040015@std.uestc.edu.cn>
2019-09-23 09:52:02 +0800

    Create installnkn.sh

---
+++ installnkn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+installNkn(){
+rm -rf /tmp/linux*
+wget https://raw.githubusercontent.com/sxzcy/nkn-install/master/team/xxhjkl.sh -O /tmp/nkninstall.sh
+chmod +x /tmp/nkninstall.sh
+bash /tmp/nkninstall.sh
+}
+installBbr(){
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh -O /tmp/bbrinstall.sh
+chmod +x /tmp/bbrinstall.sh
+bash /tmp/bbrinstall.sh
+1
+}
+
+installNkn
+installBbr
