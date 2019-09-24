3c9ddb721ca5db98874224bd472fd0546653ef53
xxhjkl <2010231040015@std.uestc.edu.cn>
2019-09-24 20:18:43 +0800

    Update autoinstallnkn.sh

--- autoinstallnkn.sh
+++ autoinstallnkn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cat $1 | while read ip pass
+cat $1 | while read ip
 do
-    export SSHPASS=$pass
+    export SSHPASS=$2
     sshpass -e ssh -n -t -t -o ConnectTimeout=2 -o StrictHostKeyChecking=no root@$ip '
 wget https://raw.githubusercontent.com/sxzcy/nkn-install/master/team/xxhjkl.sh -O /tmp/nkninstall.sh
 chmod +x /tmp/nkninstall.sh
