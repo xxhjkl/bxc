92960db9616ff119669aec1b3605521c12ca4b5f
xxhjkl <2010231040015@std.uestc.edu.cn>
2019-09-24 15:36:39 +0800

    Create autobxcr.sh

---
+++ autobxcr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat $1 | while read mac ip
+do
+    export SSHPASS=$2
+    sshpass -e ssh -n -t -t -o ConnectTimeout=2 -o StrictHostKeyChecking=no root@$ip '
+echo mac='$mac'
+sleep 1
+name=$(curl -sL ftp://xxhjkl:'$3'@img.xxhjkl.me:21/bcode/ | grep '$mac'|awk "{print \$9}")
+echo name=$name
+sleep 5
+recoverBxc() {
+ wget ftp://xxhjkl:'$3'@img.xxhjkl.me:21/bcode/$name -O 1.tar.gz && tar -zxvf 1.tar.gz
+ sleep 3
+ wget --no-check-certificate https://raw.githubusercontent.com/xxhjkl/bxc/master/bxcr.sh
+ bash bxcr.sh
+}
+check() {
+ if [ -z "$name" ] ; then
+ exit
+ else 
+ recoverBxc
+ fi
+}
+check
+' & done
